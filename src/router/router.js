aca70be181c30dcca9ba2817d2a93a6008ec9925
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-13 23:42:56 +0330

    add router

--- src/router/router.js
+++ src/router/router.js
@@ -2,14 +2,27 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Store from '../Components/Store/Store.jsx';
 import ProductDetails from "../Components/ProductDetails/ProductDetails.jsx";
+import Layout from '../Components/layout'
+import ShopCart from "../Components/ShopCart/ShopCart.jsx";
 const router = createBrowserRouter([
+  
     {
-        path: "/products",
-        element: <Store/>,
-    },
-    {
-        path:'/products/:productId',
-        element:<ProductDetails />,
-    }
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/products",
+                element: <Store/>,
+            },
+            {
+                path:'/products/:productId',
+                element:<ProductDetails />,
+            },
+            {
+                path:'/Cart',
+                element:<ShopCart/>,
+            }
+        ],
+      },
 ]);
 export default router
