582003b3d93dd9852744b57c51cf3f19c398381e
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-11-17 23:34:32 +0330

    change style store

--- src/Components/Store/Store.jsx
+++ src/Components/Store/Store.jsx
@@ -9,7 +9,7 @@ import styles from "./Store.module.css";
 const Store = () => {
     const Products =useContext(ProductsContext)
     return (
-        <Grid className={styles.container}>
+        <Grid display={'flex'} flexWrap={'wrap'} justifyContent={'space-around'}>
             {
               Products.map(product=><Product key={product.id} productData={product}/>)  
             }
