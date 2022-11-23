087d5f2e56675e4c68023a313fd890b42be537c4
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2022-11-23 12:10:10 +0330

    change router

--- src/router/router.js
+++ src/router/router.js
@@ -7,7 +7,7 @@ import ShopCart from "../Components/ShopCart/ShopCart.jsx";
 const router = createBrowserRouter([
   
     {
-        path: "/",
+        path: "/products",
         element: <Layout />,
         children: [
             {
